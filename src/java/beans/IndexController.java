2ebf6aee2915a805a18012ca1654864ce4a43e40
Helen Nagel <hnagel413@gmail.com>
2021-02-10 10:02:21 +0200

    Initial commit.

--- src/java/beans/IndexController.java
+++ src/java/beans/IndexController.java
@@ -4,43 +4,65 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.*;
-import models.Human;
+import javax.annotation.Resource;
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.UserTransaction;
+import models.Address;
 
 @Named(value = "indexController")
 @SessionScoped
 public class IndexController implements Serializable {
     
-    private Human human = new Human();
-    private List<Human> humans = new ArrayList();
+    @PersistenceUnit EntityManagerFactory emf;
+    @PersistenceContext EntityManager em;
+    @Resource UserTransaction ut;    
+    
+    private Address human = new Address();
+    private List<Address> humans = new ArrayList();
 
     public IndexController() {
     }
     
-    public List<Human> getHumans(){
+    public List<Address> getHumans(){
+        try {
+            CriteriaQuery crit = em.getCriteriaBuilder().createQuery();
+            crit.select(crit.from(Address.class));
+            Query qr = em.createQuery(crit);
+            return humans = qr.getResultList();
+        } catch (Exception e) {
+        }
         return humans;
     }
     
     public void add(){
-        if(human.isLegal()){
-            humans.add(human);
-            human = new Human();
+        try {
+            ut.begin();
+            em.persist(human);
+            ut.commit();
+            human = new Address();
+        } catch (Exception exe) {
+            try {
+                ut.rollback();
+            } catch (Exception e) {
+            }
         }
     }    
     
     public String getFirstName() {
-        return human.getFirstName();
+        return human.getFirstname();
     }
 
     public void setFirstName(String firstName) {
-        human.setFirstName(firstName);
+        human.setFirstname(firstName);
     }
 
     public String getLastName() {
-        return human.getLastName();
+        return human.getLastname();
     }
 
     public void setLastName(String lastName) {
-        human.setLastName(lastName);
+        human.setLastname(lastName);
     }
 
     public String getAddress() {
---
+++ src/java/models/Address.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author helen.nagel
+ */
+@Entity
+@Table(name = "ADDRESS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Address.findAll", query = "SELECT a FROM Address a")
+    , @NamedQuery(name = "Address.findById", query = "SELECT a FROM Address a WHERE a.id = :id")
+    , @NamedQuery(name = "Address.findByFirstname", query = "SELECT a FROM Address a WHERE a.firstname = :firstname")
+    , @NamedQuery(name = "Address.findByLastname", query = "SELECT a FROM Address a WHERE a.lastname = :lastname")
+    , @NamedQuery(name = "Address.findByAddress", query = "SELECT a FROM Address a WHERE a.address = :address")
+    , @NamedQuery(name = "Address.findByCode", query = "SELECT a FROM Address a WHERE a.code = :code")
+    , @NamedQuery(name = "Address.findByCity", query = "SELECT a FROM Address a WHERE a.city = :city")
+    , @NamedQuery(name = "Address.findByEmail", query = "SELECT a FROM Address a WHERE a.email = :email")
+    , @NamedQuery(name = "Address.findByDate", query = "SELECT a FROM Address a WHERE a.date = :date")
+    , @NamedQuery(name = "Address.findByTitle", query = "SELECT a FROM Address a WHERE a.title = :title")})
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Size(max = 50)
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+    @Size(max = 50)
+    @Column(name = "LASTNAME")
+    private String lastname;
+    @Size(max = 50)
+    @Column(name = "ADDRESS")
+    private String address;
+    @Size(max = 5)
+    @Column(name = "CODE")
+    private String code;
+    @Size(max = 50)
+    @Column(name = "CITY")
+    private String city;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 50)
+    @Column(name = "EMAIL")
+    private String email;
+    @Size(max = 10)
+    @Column(name = "DATE")
+    private String date;
+    @Size(max = 50)
+    @Column(name = "TITLE")
+    private String title;
+
+    public Address() {
+    }
+
+    public Address(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Address)) {
+            return false;
+        }
+        Address other = (Address) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Address[ id=" + id + " ]";
+    }
+    
+}
